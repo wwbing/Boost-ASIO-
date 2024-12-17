dafa4d1cbcd7665b925f337aad061bcb2541bfc2
jiahao_windows <2315363128@mail.wtu.edu.cn>
2024-12-17 15:54:35 +0800

    asio客户端

---
+++ asio_tcp_client.cpp
@@ -0,0 +1,14 @@
+﻿// asio_tcp_client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace std;
+using namespace boost;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+

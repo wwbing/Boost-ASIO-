9120f3085438041b1343e5616c7813f4d118b646
jiahao_windows <2315363128@mail.wtu.edu.cn>
2024-12-17 16:19:31 +0800

    initial commit

---
+++ asio_tcp_server.cpp
@@ -0,0 +1,15 @@
+﻿// asio_tcp_server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace std;
+using namespace boost;
+
+const int MAX_LENGTH = 1024;
+
+int main()
+{
+    cout << "Hello sync-server!\n";
+}
